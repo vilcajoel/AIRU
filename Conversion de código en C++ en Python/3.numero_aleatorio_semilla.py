0dbbab58764c801aee6ed1b573eb9a36b7407b27
Joel Vilca <joel.vilca1@unmsm.edu.pe>
2020-10-07 23:34:05 -0500

    Ejercicio 3

---
+++ Conversion de código en C++ en Python/3.numero_aleatorio_semilla.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  7 22:44:56 2020
+
+@author: vilcajoal - Vilca Joel Alberto
+"""
+# =============================================================================
+#                           CÓDIGO C++
+# =============================================================================
+# SETW(): agrega espacios vacios recibe como parametro un entero 
+# RAND(): devuelve un numero aleatorio "1+rand()%6" en el rango de 1 y 6
+# =============================================================================
+#    #include <iostream>
+#    #include <iomanip>
+#    #include <cstdlib>// contiene srand y rand
+#    using namespace std;
+#    int main()
+#    {
+#       unsigned semilla;
+#       cout << "Introduzca semilla: ";
+#       cin >> semilla;
+#       srand( semilla );
+#       for ( int contador = 1; contador <= 10; contador++ ) 
+#       {
+#       		
+#            cout << setw( 10 ) << ( 1 + rand() % 6 );
+#            
+#          	if ( contador % 5 == 0 )
+#            	 cout << endl;
+#        	
+#    		 
+#       } 
+#       return 0;  
+#    }
+
+# =============================================================================
+
+# =============================================================================
+#                           CÓDIGO PYTHON
+# =============================================================================
+
+import random
+# from random import randint
+
+def start():
+    semilla = int(input("Ingrese semilla "))
+    random.seed(semilla)
+    for contador in range(1, 11):
+        print("".ljust(10,) , random.randint(1,6), end = "")
+        if contador % 5 == 0:
+            print("")
+        
+
+    
+if __name__ == '__main__':
+    start()
+    
+    
+    
+    
+    
+    
+    
